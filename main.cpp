5bd15da8e48305ba26e16d9263d66ff0bcb1e8ea
Christopher King <G.nius.ck@gmail.com>
2014-09-24 12:31:11 -0400

    Initial Commit

---
+++ main.cpp
@@ -0,0 +1,19 @@
+// datatest.cpp
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    short x;
+
+    cout << "Enter a value: ";
+    cin >> x;
+
+    cout << "The value you entered is: ";
+    cout << x << endl;
+    return 0;
+}
+
+//Limit is 32768
